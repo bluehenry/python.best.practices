79f3dbf5cfa368be96ba51fb5373c051133e8c10
Henry Zhang <jijun.henry@gmail.com>
2020-11-24 21:23:48 +0800

    finish load pages

--- 31_pycharm/PageSpider/page_spider.py
+++ 31_pycharm/PageSpider/page_spider.py
@@ -1,13 +1,23 @@
 import os
 import argparse
+from utilities import url_utilities
+import ssl
 
 
 def main(database: str, url_list_file: str):
+    big_word_list = []
     print("we are going to work with " + database)
     print("we are going to scan" + url_list_file)
-
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading " + url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents=page_content)
+        big_word_list.extend(words)
+    print(big_word_list)
 
 if __name__ == "__main__":
+    ssl._create_default_https_context = ssl._create_unverified_context
     parser = argparse.ArgumentParser()
     parser.add_argument("-db", "--database", help="SQLite File Name")
     parser.add_argument("-i", "--input", help="File containing urls to read")
